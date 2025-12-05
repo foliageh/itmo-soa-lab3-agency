3c015b1b2caa50b8a5d85a9bd88ad362009ebbc3
foliageh <mygazztop1@gmail.com>
2025-12-05 21:53:30 +0300

    communication with agency service

--- agency-service/src/main/java/com/foliageh/controller/dto/FlatsFilterDto.java
+++ agency-service/src/main/java/com/foliageh/controller/dto/FlatsFilterDto.java
@@ -1,45 +1,22 @@
 package com.foliageh.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import com.foliageh.repository.entity.Furnish;
 import com.foliageh.repository.entity.Transport;
+import lombok.Builder;
+import lombok.Value;
 
-@Schema(description = "Запрос на сортировку квартир")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@AllArgsConstructor
-@Builder
-@Data
-public class FlatsFilterDto {
-    @JsonProperty("name")
-    @Schema(title = "Имя", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private String name;
+import java.io.Serializable;
 
-    @JsonProperty("min_area")
-    @Schema(title = "минимальная площадь", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private int minArea;
-
-    @JsonProperty("max_area")
-    @Schema(title = "минимальная площадь", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private int maxArea;
-
-    @JsonProperty("min_rooms")
-    @Schema(title = "минимальная площадь", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private int minRooms;
-
-    @JsonProperty("max_rooms")
-    @Schema(title = "максимальная площадь", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private int maxRooms;
-
-    @JsonProperty("furnish")
-    @Schema(title = "фурнитруа", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private Furnish furnish;
-
-    @JsonProperty("transport")
-    @Schema(title = "транспорт", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    private Transport transport;
+@Value @Builder
+public class FlatsFilterDto implements Serializable {
+    String name;
+    Integer min_area;
+    Integer max_area;
+    Integer min_rooms;
+    Integer max_rooms;
+    Integer min_price;
+    Integer max_price;
+    Furnish furnish;
+    Transport transport;
+    Boolean has_balcony;
 }
--- agency-service/src/main/java/com/foliageh/service/AgencyService.java
+++ agency-service/src/main/java/com/foliageh/service/AgencyService.java
@@ -1,35 +1,55 @@
 package com.foliageh.service;
 
-import javax.transaction.Transactional;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import com.foliageh.repository.FlatRepository;
+import com.foliageh.controller.dto.FlatPageResponse;
+import com.foliageh.controller.dto.FlatsFilterDto;
 import com.foliageh.repository.entity.Flat;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import javax.transaction.Transactional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class AgencyService {
-    private FlatRepository flatRepository;
-
-    public Optional<Flat> findWithBalcony(boolean cheapest, boolean withBalcony) {
-        List<Flat> flats;
-        if (cheapest) {
-            flats = flatRepository.findByHasBalconyOrderByPriceAsc(withBalcony);
-        } else {
-            flats = flatRepository.findByHasBalconyOrderByPriceDesc(withBalcony);
-        }
-        return flats.stream().findFirst();
-    }
+    private final WebClient webClient;
 
     public Optional<Flat> getMostExpensiveAmongThree(Integer id1, Integer id2, Integer id3) {
         List<Integer> ids = Arrays.asList(id1, id2, id3);
-        List<Flat> flats = flatRepository.findByIdIn(ids);
+        List<Flat> flats = ids.stream().map(id ->
+                webClient.get()
+                        .uri("/flats/{id}", id)
+                        .retrieve()
+                        .bodyToMono(Flat.class)
+                        .block()
+        ).filter(Objects::nonNull).collect(Collectors.toList());
         return flats.stream().max(Comparator.comparingDouble(Flat::getPrice));
+    }
+
+    public Optional<Flat> findWithBalcony(boolean cheapest, boolean withBalcony) {
+        FlatsFilterDto filter = FlatsFilterDto.builder().has_balcony(withBalcony).build();
+
+        FlatPageResponse page = webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/flats/filter")
+                        .queryParam("sortBy", "price")
+                        .queryParam("sortDirection", cheapest ? "asc" : "desc")
+                        .queryParam("pageNumber", 0)
+                        .queryParam("pageSize", 1)
+                        .build()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(filter)
+                .retrieve()
+                .bodyToMono(FlatPageResponse.class)
+                .block();
+
+        if (page != null && !page.getFlats().isEmpty())
+            return Optional.of(page.getFlats().get(0));
+        return Optional.empty();
     }
 }
